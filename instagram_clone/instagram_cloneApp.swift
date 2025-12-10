cda9c7d180f6d871b97b3ee5a17a10c343e7d730
ismail-ilm66 <ismail.saleem@devsinc.com>
2025-12-10 19:46:38 +0500

    Initial Commit

---
+++ instagram_clone/instagram_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_cloneApp.swift
+//  instagram_clone
+//
+//  Created by Dev on 10/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct instagram_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ instagram_cloneTests/instagram_cloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_cloneTests.swift
+//  instagram_cloneTests
+//
+//  Created by Dev on 10/12/2025.
+//
+
+import Testing
+@testable import instagram_clone
+
+struct instagram_cloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
